b909cc838a94e9e4c595a79a80e1ee3f83a3b68f
Yasmn <yasmina.osman@mail.utoronto.ca>
2014-10-05 11:07:43 -0400

    Exercise 3`
    Beginning of Exercise 3

--- exercise3.py
+++ exercise3.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 
+from random import choice
+input ()
 
-def decide_rps(player1, player2):
-    return 1
+name = input("Enter your name : ")
 
+print ("Rock breaks Scissors, Scissors cuts Paper, Paper beats Rock")
+rps = ['r','p','s']
+player1 = 0
+player2 = 0
+
+print ("R: Rock,      P: Paper,     S: Scissor")
+
+user = input("Enter your choice among the three : ")
+user = user.lower()
+
+#Here is the choice()
+py = choice(rps)
