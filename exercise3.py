5298e38bab722f16e9a5630cfb03fd702650e8ab
Yasmn <yasmina.osman@mail.utoronto.ca>
2014-10-05 15:08:38 -0400

    Results for each Entry (Exercise 3)
    Determining the rock, paper and scissors result in a dictionary

--- exercise3.py
+++ exercise3.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python3
 
-from random import choice
-input ()
+#this is to designate the results for each entry
+decide_rps = {}
+decide_rps [("rock" "paper")] = 2
+decide_rps [("rock" "scissors")] = 1
+decide_rps [("rock" "rock")] = 0
 
-name = input("Enter your name : ")
+decide_rps [("scissors" "paper")] = 1
+decide_rps [("scissors" "rock")] = 2
+decide_rps [("scissors" "scissors")] = 0
 
-print ("Rock breaks Scissors, Scissors cuts Paper, Paper beats Rock")
-rps = ['r','p','s']
-player1 = 0
-player2 = 0
+decide_rps [("paper" "scissors")] = 2
+decide_rps [("paper" "rock")] = 1
+decide_rps [("paper" "paper")] = 0
 
-print ("R: Rock,      P: Paper,     S: Scissor")
-
-user = input("Enter your choice among the three : ")
-user = user.lower()
-
-#Here is the choice()
-py = choice(rps)
